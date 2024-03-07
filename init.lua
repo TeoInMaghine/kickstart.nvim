3b2272310137ccb2e6a1c80f59b76a290f826d32
TeoInMaghine <teopiratoguthmann2004@gmail.com>
2024-03-07 11:20:24 -0300

    Added trouble.nvim and mappings

--- init.lua
+++ init.lua
@@ -356,7 +356,9 @@ end, 0)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = '[Q]uick-fix list' })
+vim.keymap.set("n", "<leader>dw", function() require("trouble").toggle("workspace_diagnostics") end, { desc = '[W]orkspace [D]iagnostics' })
+vim.keymap.set("n", "<leader>dd", function() require("trouble").toggle("document_diagnostics") end, { desc = '[D]ocument [D]iagnostics' })
+-- vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { desc = '[D]iagnostics quick-fix list' })
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
@@ -383,7 +385,7 @@ local on_attach = function(_, bufnr)
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
   -- nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  -- nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   -- nmap('<leader>k', vim.lsp.buf.hover, 'Hover Documentation')
@@ -411,7 +413,7 @@ require('which-key').register {
   ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 }
 
 -- mason-lspconfig requires that these setup functions are called in this order
