c14f1628a38659fbf961f43908fe9202d5187c6a
TeoInMaghine <59812116+TeoInMaghine@users.noreply.github.com>
2024-03-18 15:39:24 -0300

    H and L now work like Home and End

--- lua/custom/mappings/navigation.lua
+++ lua/custom/mappings/navigation.lua
@@ -5,9 +5,30 @@ vim.keymap.set({ 'n', 'v' }, 'K', '5kzz', { silent = true })
 vim.keymap.set('n', '<leader>j', 'J', { silent = true })
 
 -- More comfy home and end of line
-vim.keymap.set({ 'n', 'v', 'o' }, 'L', 'g_', { silent = true })
-vim.keymap.set({ 'n', 'v' }, 'H', '^ze', { silent = true })
+vim.keymap.set({ 'n', 'v' }, 'H', function() LineHome() end, { silent = true })
 vim.keymap.set({ 'o' }, 'H', '^', { silent = true })
+vim.keymap.set({ 'n', 'v' }, 'L', function() LineEnd() end, { silent = true })
+vim.keymap.set({ 'o' }, 'L', 'g_', { silent = true })
+
+-- Makes H work like Home
+function LineHome()
+    local x = vim.fn.col('.')
+    vim.cmd("normal ^")
+    if x == vim.fn.col('.') then
+        vim.cmd("normal 0")
+    end
+    return ""
+end
+
+-- Makes L work like End
+function LineEnd()
+    local x = vim.fn.col('.')
+    vim.cmd("normal g_")
+    if x == vim.fn.col('.') then
+        vim.cmd("normal $")
+    end
+    return ""
+end
 
 -- After gg or G, be able to use 'g to jump back
 vim.keymap.set({ 'n', 'v' }, 'gg', 'mggg', { silent = true })
