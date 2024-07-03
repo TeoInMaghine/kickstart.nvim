6a7eebc2f619125b7087367e62344c94e586f56b
TeoInMaghine <teopiratoguthmann2004@gmail.com>
2024-07-03 08:44:13 -0300

    Disable spider's skipInsignificantPunctuation

--- lua/custom/plugins/spider.lua
+++ lua/custom/plugins/spider.lua
@@ -1,6 +1,7 @@
 return {
     {
         "chrisgrieser/nvim-spider",
+        skipInsignificantPunctuation = false,
         keys = {
             -- Lazy loading on keystroke, .setup() call is optional
             {
