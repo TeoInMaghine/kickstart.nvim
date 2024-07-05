84885a19cd434e8e921373fd8d74f6c62df8492f
TeoInMaghine <teopiratoguthmann2004@gmail.com>
2024-07-05 12:28:38 -0300

    Removed unnecessary which-key mapping description

--- init.lua
+++ init.lua
@@ -436,7 +436,6 @@ require('which-key').register {
   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   ['<leader>d'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
   ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
 }
 
