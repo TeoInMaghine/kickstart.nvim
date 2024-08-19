942b44ca673c25e8edcd8dc76bfe3f69924d69f1
TeoInMaghine <teopiratoguthmann2004@gmail.com>
2024-08-19 11:25:05 -0300

    Robot spell checking

--- queries/robot/highlights.scm
+++ queries/robot/highlights.scm
@@ -2,4 +2,6 @@
 
 ; This resource is GOATED: https://tree-sitter.github.io/tree-sitter/using-parsers
 
-(keyword_definition (body . (keyword_setting) @spell . (statement)* @spell))
+; This matches the first keyword_setting and any following nodes with "    ..." that are inside
+; a keyword_definition body; it matches keyword docs and marks it as @spell
+(keyword_definition (body . (keyword_setting) @spell . (#match? @spell "    \.\.\.")))
